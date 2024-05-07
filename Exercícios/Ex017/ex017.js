19f885c4f18053c678ebb4b911c5a863811b1b6c
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-05-07 12:41:23 -0300

    ex while

--- Exercícios/Ex017/ex017.js
+++ Exercícios/Ex017/ex017.js
@@ -1,6 +1,17 @@
-var i = 1
+//var i = 1
+//
+//do { 
+//    console.log(`td bem? ${i}`)
+//    i++
+//} while (i <= 10)
 
-while (i <= 10) {
-    console.log(`td bem? ${i}`)
-    i++
+var nomes = ['Abelha','Arvore','Ameixa']
+var salario = 1200
+
+console.log(`Seu nome é ${nomes[2]}`)
+console.log(`Seu salário é ${salario}`)
+while (salario < 2000) {
+    salario += 200
+    console.log(`VOCÊ RECEBEU UM AUMENTO DE 200 R$`)
 }
+console.log(`Seu novo salário é de ${salario}`)
