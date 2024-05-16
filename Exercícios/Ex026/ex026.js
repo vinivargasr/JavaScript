d8b18580b580a18553f2cecb8bbfdb0b4ee1ee11
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-05-16 15:06:38 -0300

    tipos de function

---
+++ Exercícios/Ex026/ex026.js
@@ -0,0 +1,36 @@
+/*
+function soma (...valores) {
+    let tam = valores.length
+    let res = 0
+    for(let v of valores) {
+        res += v
+    }
+
+    return res
+}
+
+console.log(soma(10, 5, 2, 15, 8))
+
+----------------------------------------------
+
+const f = new Function('v1', 'v2', 'return v1 + v2') // Função Construtor Anônima
+
+console.log(f(10, 5))
+
+----------------------------------------------
+
+const soma = (v1, v2) => {
+    let res = v1 + v2
+    return res
+}
+console.log(soma(10, 5))
+
+const nome = n => n
+console.log(nome('Bruno'))
+
+const add = n => n + 10
+console.log(add(10))
+*/
+
+
+
