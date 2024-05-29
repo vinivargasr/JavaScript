ae14d91d260d596efabc6b05b51679b666704a58
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-05-29 14:40:58 -0300

    ex método map

--- Exercícios/Ex035/ex035.js
+++ Exercícios/Ex035/ex035.js
@@ -11,3 +11,24 @@ const conveterInt = (e) => parseInt(e)
 const dobrar = (e) => e*2
 let num = ['1','2','3','4','5'].map(dobrar)
 console.log(num)
+
+// -----------------------------]
+
+const numbers = [1, 4, 9, 16];
+const doubled = numbers.map(num => num * 2);
+
+console.log(doubled); // [2, 8, 18, 32]
+
+
+// -----------------------------]
+
+const usuarios = [
+    { primeiroNome: 'John', ultimoNome: 'Doe'},
+    { primeiroNome: 'Jane', ultimoNome: 'Doe'},
+    { primeiroNome: 'Sara', ultimoNome: 'Smith'},
+    { primeiroNome: 'Michael', ultimoNome: 'Jackson'},
+]
+
+const nomeInteiro = usuarios.map(usuario => `${usuario.primeiroNome} ${usuario.ultimoNome}`)
+
+console.log(nomeInteiro)
