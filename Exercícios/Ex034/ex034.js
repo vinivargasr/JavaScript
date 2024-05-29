14901ee8c72caf6ec1c328e28e82d97fc99811b5
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-05-29 13:40:28 -0300

    function geradora

---
+++ Exercícios/Ex034/ex034.js
@@ -0,0 +1,33 @@
+function* cores() {
+    yield 'Vermelho'
+    yield 'Verde'
+    yield 'Azul'
+}
+
+const itc = cores()
+console.log(itc.next().value)
+console.log(itc.next().value)
+console.log(itc.next().value)
+
+
+function* perguntas() {
+    const nome = yield 'Qual seu nome?'
+    const esporte = yield 'Qual seu esporte favorito?'
+    return `Seu nome é ${nome}, seu esporte favorito é ${esporte}`
+}
+
+const itp = perguntas()
+console.log(itp.next().value)
+console.log(itp.next('Bruno').value)
+console.log(itp.next('Natação').value)
+
+
+function* contador() {
+    let i = 0
+    while(true) {
+        yield i++
+    }
+}
+
+const itq = contador()
+console.log(itq.next().value)
