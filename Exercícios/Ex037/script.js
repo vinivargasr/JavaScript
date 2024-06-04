ed1563c68c7c05e84c83bacc6237881ed5dd1d04
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-06-04 16:28:42 -0300

    events

--- Exercícios/Ex037/script.js
+++ Exercícios/Ex037/script.js
@@ -3,7 +3,21 @@ var cursos1 = [...document.getElementsByClassName('c1')]
 var cursos2 = [...document.getElementsByClassName('c2')]
 var cursoEspecial = document.getElementsByClassName('curso')[0]
 
+//cursos2.map((el) => {
+//    el.classList.add('destaque')
+//})
 
-cursos2.map((el) => {
-    el.classList.add('destaque')
+// ---------------------------------------------
+
+const cursos = [...document.querySelectorAll('.curso')]
+
+cursos.map((el) => {
+    el.addEventListener('click',(evt) => {
+        const el = evt.target
+        el.classList.add('destaque')
+    })
 })
+
+const msg = () => {
+    alert('clicou')
+}
