bc36f1198de214327f33d410ed9546afc35985f2
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-06-07 16:29:29 -0300

    arrumando ex, para apenas 1 button

--- Exercícios/Ex038/ex038.js
+++ Exercícios/Ex038/ex038.js
@@ -1,7 +1,6 @@
 const caixa1 = document.getElementById('caixa1')
 const caixa2 = document.getElementById('caixa2')
-const botao = document.getElementById('btn_copiar')
-const botao2 = document.getElementById('btn_voltar')
+const botao = document.getElementById('btn_transferir')
 const cursos = [...document.querySelectorAll('.curso')]
 
 cursos.map((el)=>{
@@ -13,14 +12,13 @@ cursos.map((el)=>{
 
 botao.addEventListener('click', () => {
     const cursosSelecionados = [...document.querySelectorAll('.selecionado')]
+    const cursosNaoSelecionados = [...document.querySelectorAll('.curso:not(.selecionado)')]
+
     cursosSelecionados.map((el) => {
         caixa2.appendChild(el)
     })
-})
 
-botao2.addEventListener('click', () => {
-    const cursosSelecionados = [...document.querySelectorAll('.selecionado')]
-    cursosSelecionados.map((el) => {
+    cursosNaoSelecionados.map((el) => {
         caixa1.appendChild(el)
     })
 })
