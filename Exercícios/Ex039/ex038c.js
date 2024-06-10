590a05dfc9d639f1246cb619a9ffe3ce8b4b7d0a
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-06-10 15:31:56 -0300

    utilizando appendChild /removeChild / setAttribute

--- Exercícios/Ex039/ex038c.js
+++ Exercícios/Ex039/ex038c.js
@@ -9,5 +9,14 @@ cursos.map((el, chave) => {
     newElement.setAttribute('class','curso c1')
     newElement.innerHTML = el
 
+    const btn_lixeira = document.createElement('img')
+    btn_lixeira.setAttribute('src','lixo.png')
+    btn_lixeira.setAttribute('class','btn_lixeira')
+    newElement.appendChild(btn_lixeira)
+
+    btn_lixeira.addEventListener('click', (evt) => {
+        caixa1.removeChild(newElement)
+    })
+
 caixa1.appendChild(newElement)
 })
