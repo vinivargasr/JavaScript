d57ea012d6a1d3bfe1337b92e9fd1e5e7c6c0d14
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-06-24 14:34:33 -0300

    teste

---
+++ Exercícios/Ex045/socket.io.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on *:3000');
+});
