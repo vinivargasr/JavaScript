49b9bd2f0ae2aa884b82971e0bc8ddf623093da5
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-06-28 14:54:45 -0300

    coleções Set() e Map()

---
+++ Exercícios/Ex047/script3.js
@@ -0,0 +1,31 @@
+const caixa = document.getElementById('caixa')
+
+// let mapa = new Map()
+
+// mapa.set('Curso', 'Javascript')
+// mapa.set(10, 'CFB Cursos')
+// mapa.set(1, 100)
+// mapa.set('Canal', 100)
+
+// console.log(mapa)
+
+// let pes = 10
+// if (mapa.has(pes)) {
+//     caixa.innerHTML = `A chave existe, com o value: ${mapa.get(pes)}`
+// } else {
+//     caixa.innerHTML = 'A chave não exsite'
+// }
+
+let musicas = new Set(['musica1', 'musica7', 'musica10'])
+
+musicas.add('musica4')
+
+console.log(musicas)
+
+// musicas.forEach((el) => {
+//     caixa.innerHTML += `${el}<br>`
+// })
+
+for (let m of musicas) {
+    caixa.innerHTML += `${m}<br>`
+}
