2cf083333d3bd2e1282ee4225b9914017c9330ff
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-08-20 17:04:41 -0300

    Utilizando Static em Objeto

---
+++ Exercícios/Ex052/scriptb.js
@@ -0,0 +1,25 @@
+class Npc {
+    static alerta = false
+    constructor(energia) {
+        this.energia = energia
+    }
+
+    info = function() {
+        console.log(`Energia: ${this.energia}`)
+        console.log(`Alerta: ${Npc.alerta?'Sim':'Não'}`)
+        console.log('------------------------')
+    }
+    static alertar = function () {
+        Npc.alerta = true
+    }
+}
+
+Npc.alertar()
+
+const npc1 = new Npc(100)
+const npc2 = new Npc(80)
+const npc3 = new Npc(30)
+
+npc1.info()
+npc2.info()
+npc3.info()
