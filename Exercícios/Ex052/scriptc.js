c2ac44c05b0462e80b109030f127c8dd6e06e321
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-09-02 10:24:34 -0300

    utilizando prototype

---
+++ Exercícios/Ex052/scriptc.js
@@ -0,0 +1,18 @@
+const Nave = function(energia){
+    this.energia = energia
+    this.disparos = 100
+}
+
+const n1 = new Nave(100)
+
+Nave.prototype.vidas = 3
+Nave.prototype.disparar = function() {
+    if (this.disparos > 0) {
+        this.disparos--
+    }
+}
+
+n1.disparar()
+
+console.log(n1.vidas)
+console.log(n1.disparos)
