4b58f032880d5e23e0f4ce5019c05ccd0b4dc231
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-09-18 11:06:31 -0300

    metodos do window.location

---
+++ Exercícios/Ex058/script.js
@@ -0,0 +1,7 @@
+const url = document.getElementById('url')
+const btnUrl = document.getElementById('btnUrl')
+
+btnUrl.addEventListener('click',(evt) => {
+    // window.location = "https://www.google.com"
+    window.location.replace('https://www.google.com')
+})
