ce3139978577a97c7d9b72f8e0df335d1a2b49a0
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-09-19 09:52:53 -0300

    window.location

--- Exercícios/Ex058/script.js
+++ Exercícios/Ex058/script.js
@@ -3,5 +3,12 @@ const btnUrl = document.getElementById('btnUrl')
 
 btnUrl.addEventListener('click',(evt) => {
     // window.location = "https://www.google.com"
-    window.location.replace('https://www.google.com')
+    // window.location.replace('https://www.google.com')
+    // window.location.assign('https://www.google.com')
+    // window.location.reload()
+    // window.history.back()
+    // window.history.forward()
+    // window.history.go()
+    // console.log(window.history.length)
+    window.location = url.value
 })
