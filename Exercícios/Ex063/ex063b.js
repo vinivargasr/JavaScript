0897026c5d0392b7e3c4cc6563725e7e7f188172
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-10-01 10:19:54 -0300

    Expressões regulares - RegEx

--- Exercícios/Ex063/ex063b.js
+++ Exercícios/Ex063/ex063b.js
@@ -1,12 +1,10 @@
-let nome = new String('Bruno')
+let nome = new String('Vinícius Vargas Ribeiro')
 let nome2 = new String('Bruno')
 let canal = new String('CBF')
 
 console.log(nome.charAt(0))
 console.log(nome.charCodeAt(0))
 console.log(nome.concat(canal))
-
-
 console.log(nome.localeCompare(nome2)) // Se retornar 0 é igual
 console.log(nome.replace('Bruno','Paulo')) // Substitui String
 console.log(nome.search('Pinho'))
@@ -18,3 +16,13 @@ console.log(nome.endsWith('Campos')) // Termina com, true ou false
 console.log(nome.includes('teste')) // contém na string, true ou false
 console.log(nome.repeat(3)) // Repete a string, pela quantidade definida
 console.log(String.fromCodePoint(66))
+
+// RegEx - Expressões Regulares
+// g: Busca global (encontra todas as ocorrências, não só a primeira).
+// i: Ignora maiúsculas e minúsculas.
+// m: Busca em múltiplas linhas.
+console.log(nome.search(/vargas/i))
+console.log(nome.match(/v/ig))
+console.log(nome.replace(/vargas/ig, 'Testando'))
+console.log(nome.match(/[ri]/ig))
+console.log(nome.match(/[a-j]/ig))
