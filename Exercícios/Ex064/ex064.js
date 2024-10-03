18428ec4994fbbc2d4e632e3fd2ddab9bc42be51
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-10-03 11:11:23 -0300

    animações

--- Exercícios/Ex064/ex064.js
+++ Exercícios/Ex064/ex064.js
@@ -3,25 +3,49 @@ const btnEsquerda = document.getElementById('btnEsquerda')
 const btnDireita = document.getElementById('btnDireita')
 const btnParar = document.getElementById('btnParar')
 
-let posiçãoCarro = 0
-let cliquesRealizados = 0
+let anima = null
+let tamMax = null
+
+const init = () => {
+    carro.style = 'position:relative; left:0px'
+    tamMax = window.innerWidth - 166
+}
+
+const mover = (direcao) => {
+    if (direcao > 0) {
+        if (parseInt(carro.style.left) <= tamMax) {
+            carro.style.left = parseInt(carro.style.left) + (10*direcao) + 'px'
+            anima = setTimeout(mover, 5, direcao)
+        } else {
+            clearTimeout(anima)
+            mover(-1)
+        }
+    } else {
+        if (parseInt(carro.style.left) >= 0) {
+            carro.style.left = parseInt(carro.style.left) + (10*direcao) + 'px'
+            anima = setTimeout(mover, 5, direcao)
+        } else {
+            clearTimeout(anima)
+            mover(1)
+        }
+    }
+}
 
 btnDireita.addEventListener('click', () => {
-    if (cliquesRealizados < 1) {
-        cliquesRealizados ++
-        posiçãoCarro += 1200
-        carro.style.marginLeft = `${posiçãoCarro}px`
-    }
+    clearTimeout(anima)
+    mover(1)
 })
 
 btnEsquerda.addEventListener('click', () => {
-    if (cliquesRealizados > 0) {
-        cliquesRealizados --
-        posiçãoCarro += -1200
-        carro.style.marginLeft = `${posiçãoCarro}px`
-    }
+    clearTimeout(anima)
+    mover(-1)
 })
 
 btnParar.addEventListener('click', () => {
-    carro.style.marginLeft = `${posiçãoCarro}px`
+    clearTimeout(anima)
+})
+
+window.onload = init
+window.addEventListener('resize',() => {
+    tamMax = window.innerWidth - 166
 })
