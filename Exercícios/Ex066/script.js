1ab9ca43d41bfebb293aadaaa34d10d91cfef977
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-10-07 11:17:01 -0300

    desestruturação

---
+++ Exercícios/Ex066/script.js
@@ -0,0 +1,16 @@
+// let numreos = [10, 20, 30, 40]
+
+// let [a, b = 0, c = 0, d = 0] = [10]
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+let a = 10
+let b = 20
+
+[a, b] = [b, a]
+
+console.log(a)
+console.log(b)
