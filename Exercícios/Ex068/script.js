f5fa318bd8e107da85f03245846d01bda38302fb
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-10-16 10:40:04 -0300

    json

---
+++ Exercícios/Ex068/script.js
@@ -0,0 +1,7 @@
+const endpoint = 'https://adaf99f8-a622-4297-b5a5-23c2f0553d91-00-il64hx3ymv9t.picard.replit.dev/'
+
+fetch(endpoint)
+.then(res => res.text())
+.then(dados => {
+    console.log
+})
