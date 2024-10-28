09669cdc147676f58b41079d5000f52a282b957b
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-10-28 09:31:11 -0300

    fetch

--- Exercícios/Ex072/script.js
+++ Exercícios/Ex072/script.js
@@ -1,3 +1,8 @@
 import {Login} from './login.js'
 
+fetch('http://127.0.0.1:5500/Exerc%C3%ADcios/Ex072/sla.js')
+.then(resp => resp.json())
+.then(json => console.log(json))
+
+
 Login.login('123','321')
--- Exercícios/Ex072/sla.js
+++ Exercícios/Ex072/sla.js
@@ -1,13 +1,17 @@
-let parametros=url.parse(req.url,true);
+function login(requst, response) {
+  let parametros=url.parse(req.url,true);
 
   let mat=parametros.query.matricula;
   let pas=parametros.query.senha;
 
   let dados=null
-  
+    
   if(mat=="123" && pas=="321"){
-    dados = {
-      nome: "Bruno",
-      acesso:10
-    }
+      response.json({
+        nome: "Bruno",
+        acesso:10
+      })
   }
+}
+
+export default login
