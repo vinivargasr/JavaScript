17d1f03cba0af867961b3b9eeeba9a0a4338c73a
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2024-11-05 11:07:16 -0300

    att com nodered e wampserver

--- Exercícios/Ex073/script.js
+++ Exercícios/Ex073/script.js
@@ -1,5 +1,5 @@
 const configDGV = {
-    endpoint: 'produtos.json',
+    endpoint: 'http://localhost:1880/produtos',
     idDestino: 'ProdutosDGV'
 }
 
@@ -9,27 +9,28 @@ const datagridview = (configDGV) => {
     fetch(configDGV.endpoint)
     .then(res => res.json())
     .then(res => {
+        console.log(res)
         res.forEach(el => {
             const tr = document.createElement('tr')
             
             const td1 = document.createElement('td')
             td1.setAttribute('class', 'c1')
-            td1.innerHTML = el.id
+            td1.innerHTML = el.n_id_produto
             tr.appendChild(td1)
 
             const td2 = document.createElement('td')
             td2.setAttribute('class', 'c2')
-            td2.innerHTML = el.produto
+            td2.innerHTML = el.s_nome_produto
             tr.appendChild(td2)
 
             const td3 = document.createElement('td')
             td3.setAttribute('class', 'c3')
-            td3.innerHTML = el.marca
+            td3.innerHTML = el.s_marca_produto
             tr.appendChild(td3)
 
             const td4 = document.createElement('td')
             td4.setAttribute('class', 'c4')
-            td4.innerHTML = el.modelo
+            td4.innerHTML = el.s_modelo_produto
             tr.appendChild(td4)
 
             const td5 = document.createElement('td')
@@ -49,12 +50,21 @@ const datagridview = (configDGV) => {
             const iconDelete = document.createElement('img')
             iconDelete.setAttribute('class', 'icons')
             iconDelete.setAttribute('src', 'deleteIcon.svg')
+            iconDelete.addEventListener('click', (evt) => {
+                const id = evt.target.parentNode.parentNode.firstChild.innerHTML
+                const linha = evt.target.parentNode.parentNode
+                const endpoint = `http://localhost:1880/removeproduto/${id}`
+                fetch(endpoint)
+                .then(res => {
+                    if (res.status == 200) {
+                        linha.remove()
+                    }
+                })
+            })
             td5.appendChild(iconDelete)
 
             ProdutosDGV.appendChild(tr)
         })
-
-        console.log(res)
     })
 }
 
