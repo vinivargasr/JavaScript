644e4308eb64a6bb94fc21f7507fb37427ea7892
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2025-01-06 11:00:58 -0300

    optional chaining

---
+++ Exercícios/Ex074/script.js
@@ -0,0 +1,24 @@
+// const idade = null
+
+// console.log(`Sua idade é: ${idade ?? 'Não informado'}`)
+
+
+// const user = {
+//     name: 'Vinícius',
+//     age: 19,
+//     address: {
+//         street: 'Rua Teste',
+//         number: 176,
+//         zip: {
+//             code: '891603250',
+//             city: 'Curitiba'
+//         }
+//     }
+// }
+
+// console.log(user.address?.zip?.code ?? "Não informado")
+
+const arrayExemplo = [1, 2, 3, 4, 5]
+
+
+console.log(novoArray)
